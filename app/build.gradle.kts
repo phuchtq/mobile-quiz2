cff8de35acface4031b74feb2163a852931a3aff
phuchtq <taikhoanhoconl123@gmail.com>
2025-06-28 15:48:50 +0700

    add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,4 +42,10 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation("androidx.room:room-runtime:2.5.0")
+    annotationProcessor("androidx.room:room-compiler:2.5.0")
+
+    // Material Design
+    implementation("com.google.android.material:material:1.9.0")
 }
